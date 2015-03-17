0c19dd5d9dbdfcbd412cb6b984ed31d5c19e1547
hadley <h.wickham@gmail.com>
2015-03-17 05:07:51 -0500

    Document read_xls

--- R/read_xls.R
+++ R/read_xls.R
@@ -1,3 +1,15 @@
+#' Read an xls file (Excel 94-2007)
+#'
+#' @param path Path to the xls file
+#' @param sheet Sheet to read. Either a string (the name of a sheet), or
+#'   a integer (the position of the sheet). Defaults to the first sheet.
+#' @param col_names Either \code{TRUE} to use the first row as column names,
+#'   \code{FALSE} to number columns sequentially from \code{X1} to \code{Xn},
+#'   or a character vector giving a name for each column.
+#' @param col_types Either \code{NULL} to guess from the spreadsheet or a
+#'   character containing "blank", "numeric", "date" or "text".
+#' @param skip Number of rows to skip before reading any data.
+#' @export
 read_xls <- function(path, sheet = 1, col_names = TRUE, col_types = NULL, skip = 0) {
   path <- check_file(path)
 
