094571c6ccb9eaa071405d9cd05d6a82ad4e2187
hadley <h.wickham@gmail.com>
2015-03-17 08:24:22 -0500

    Mild refactoring of xlsx_sheets

--- src/Xlsx.cpp
+++ src/Xlsx.cpp
@@ -3,18 +3,25 @@ using namespace Rcpp;
 #include "rapidxml.h"
 using namespace rapidxml;
 
-// [[Rcpp::export]]
-std::vector<std::string> xlsx_sheets_(RawVector xml) {
+void parse_raw(xml_document<>* pDoc, RawVector xml) {
   std::string buffer;
   buffer.resize(xml.size() + 1);
   memcpy(&buffer[0], RAW(xml), xml.size());
   buffer[buffer.size() - 1] = '\0';
 
+  pDoc->parse<0>(&buffer[0]);
+}
+
+// [[Rcpp::export]]
+std::vector<std::string> xlsx_sheets_(RawVector xml) {
   xml_document<> doc;
-  doc.parse<0>(&buffer[0]);
+  parse_raw(&doc, xml);
 
   std::vector<std::string> sheetNames;
   xml_node<>* sheets = doc.first_node()->first_node("sheets");
+
+  if (sheets == NULL)
+    stop("Invalid xlsx file: no workbook/sheets tag found");
 
   for (xml_node<>* sheet = sheets->first_node(); sheet;
        sheet = sheet->next_sibling()) {
@@ -24,3 +31,4 @@ std::vector<std::string> xlsx_sheets_(RawVector xml) {
 
   return sheetNames;
 }
+
