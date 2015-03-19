b8959b6076725309cdefa0a76def885725e03182
hadley <h.wickham@gmail.com>
2015-03-19 08:07:40 -0500

    Avoid std::string allocations

--- src/XlsxCell.h
+++ src/XlsxCell.h
@@ -51,18 +51,13 @@ public:
     return location_.second;
   }
 
-  std::string typeString() {
-    rapidxml::xml_attribute<>* t = cell_->first_attribute("t");
-    return (t == NULL) ? "n" : std::string(t->value());
-  }
-
   std::string asStdString(const std::vector<std::string>& stringTable) {
     rapidxml::xml_node<>* v = cell_->first_node("v");
     if (v == NULL)
       return "[NULL]";
 
-    std::string type = typeString();
-    if (type != "s")
+    rapidxml::xml_attribute<>* t = cell_->first_attribute("t");
+    if (t == NULL || strncmp(t->value(), "s", 3) != 0)
       return std::string(v->value());
 
     int id = atoi(v->value());
@@ -92,48 +87,49 @@ public:
     if (v == NULL)
       return NA_STRING;
 
-    std::string type = typeString();
+    rapidxml::xml_attribute<>* t = cell_->first_attribute("t");
 
-    std::string string;
-    if (type == "s") {
+    if (t != NULL && strncmp(t->value(), "s", t->value_size()) == 0) {
       int id = atoi(v->value());
-      if (id < 0 || id >= stringTable.size())
-        Rcpp::stop("[%i, %i]: invalid string id '%i'", row() + 1, col() + 1, id);
-
-      string = stringTable[id];
+      const std::string& string = stringTable.at(id);
+      return (string == na) ? NA_STRING : Rf_mkCharCE(string.c_str(), CE_UTF8);
     } else {
-      string = std::string(v->value());
+      if (na.compare(v->value()) == 0) {
+        return NA_STRING;
+      } else {
+        return Rf_mkCharCE(v->value(), CE_UTF8);
+      }
     }
-
-    return (string == na) ? NA_STRING : Rf_mkCharCE(string.c_str(), CE_UTF8);
   }
 
-  CellType type(const std::string& na, const std::vector<std::string> stringTable,
+  CellType type(const std::string& na,
+                const std::vector<std::string> stringTable,
                 const std::set<int>& dateStyles) {
-    std::string type = typeString();
+    rapidxml::xml_attribute<>* t = cell_->first_attribute("t");
 
-    if (type == "b") {
-      // TODO
-      return CELL_NUMERIC;
-    } else if (type == "d") {
-      // Does excel use this? Regardless, don't have cross-platform ISO8601
-      // parser (yet) so need to return as text
-      return CELL_TEXT;
-    } else if (type == "n") {
+    if (t == NULL || strncmp(t->value(), "n", 5) == 0) {
       rapidxml::xml_attribute<>* s = cell_->first_attribute("s");
       int style = (s == NULL) ? -1 : atoi(s->value());
 
       return (dateStyles.count(style) > 0) ? CELL_DATE : CELL_NUMERIC;
-    } else if (type == "s" || type == "str") {
+    } else if (strncmp(t->value(), "b", 5) == 0) {
+      // TODO
+      return CELL_NUMERIC;
+    } else if (strncmp(t->value(), "d", 5) == 0) {
+      // Does excel use this? Regardless, don't have cross-platform ISO8601
+      // parser (yet) so need to return as text
+      return CELL_TEXT;
+    } else if (strncmp(t->value(), "s", 5) == 0 || strncmp(t->value(), "str", 5) == 0) {
       rapidxml::xml_node<>* v = cell_->first_node("v");
       if (v == NULL)
         return CELL_BLANK;
 
-      std::string string = stringTable.at(atoi(v->value()));
+      int id = atoi(v->value());
+      const std::string& string = stringTable.at(id);
       return (string == na) ? CELL_BLANK : CELL_TEXT;
     } else {
-      // I don't think Excel uses inline strings ("inlineStr")
-      Rcpp::warning("Unknown type '%s' in [%i, %i]", type, row(), col());
+      Rcpp::warning("[%i, %i]: unknown type '%s'",
+        row() + 1, col() + 1, t->value());
       return CELL_TEXT;
     }
 
