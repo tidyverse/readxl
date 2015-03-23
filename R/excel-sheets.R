49e8b5788ade2860d71cbdcf4d2f0158c34aa35c
hadley <h.wickham@gmail.com>
2015-03-23 07:29:05 -0500

    Show how to load all sheets in workbook. Closes #10

--- R/excel-sheets.R
+++ R/excel-sheets.R
@@ -5,6 +5,10 @@
 #' @examples
 #' excel_sheets(system.file("extdata/datasets.xlsx", package = "readxl"))
 #' excel_sheets(system.file("extdata/datasets.xls", package = "readxl"))
+#'
+#' # To load all sheets in a workbook, use lapply
+#' path <- system.file("extdata/datasets.xls", package = "readxl")
+#' lapply(excel_sheets(path), read_excel, path = path)
 excel_sheets <- function(path) {
   path <- check_file(path)
   ext <- tolower(tools::file_ext(path))
