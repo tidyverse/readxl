014536606bf8a44eb7e93a4978519262c7d0e1d8
hadley <h.wickham@gmail.com>
2015-03-23 09:50:33 -0500

    xlsx class to make debugging easier

--- R/zip_buffer.R
+++ R/zip_buffer.R
@@ -1,3 +1,42 @@
+xlsx <- function(path) {
+  files <- unzip(path, list = TRUE)
+  names(files) <- tolower(names(files))
+  files$date <- NULL
+  files <- files[order(files$name), ]
+  rownames(files) <- NULL
+
+  structure(list(path = path, files = files), class = "xlsx")
+}
+
+#' @export
+print.xlsx <- function(x, ...) {
+  cat("<xlsx>\n")
+  print(x$files)
+}
+
+#' @export
+`[[.xlsx` <- function(x, i, ...) {
+  if (is.numeric(i)) {
+    path <- x$files$name[[i]]
+  } else if (is.character(i)) {
+    path <- i
+  } else {
+    stop("i not supported", call. = FALSE)
+  }
+
+  ext <- tolower(tools::file_ext(path))
+
+  switch(ext,
+    xml = zip_xml(x$path, path),
+    rels = zip_xml(x$path, path),
+    zip_buffer(x$path, path)
+  )
+  invisible()
+}
+
+# ------------------------------------------------------------------------------
+
+# Called only from C++ code, but currently needs to be implemented in R.
 zip_buffer <- function(zip_path, file_path) {
   files <- utils::unzip(zip_path, list = TRUE)
 
@@ -11,8 +50,4 @@ zip_buffer <- function(zip_path, file_path) {
   con <- unz(zip_path, file_path, open = "rb")
   on.exit(close(con), add = TRUE)
   readBin(con, raw(), n = size)
-}
-
-zip_list <- function(zip_path) {
-  unzip(zip_path, list = TRUE)
 }
