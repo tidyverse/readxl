257e2e44d2c48bb4535ba14d08b10807345bdc3d
Jonathan Marshall <jmarshall@xbmc.org>
2015-04-02 17:45:59 +1300

    adds test for empty named columns that should be kept

--- tests/testthat/test-missing-values.R
+++ tests/testthat/test-missing-values.R
@@ -46,3 +46,10 @@ test_that("empty first column gives valid data.frame", {
   df <- read_excel("missing-first-column.xlsx", col_names=F)
   expect_equal(nrow(df), length(df[,1]))
 })
+
+test_that("empty named column gives NA column", {
+  df1 <- read_excel("empty-named-column.xlsx", col_names=T)
+  df2 <- read_excel("empty-named-column.xls", col_names=T)
+  expect_equal(ncol(df1), 4)
+  expect_equal(ncol(df2), 4)
+})
