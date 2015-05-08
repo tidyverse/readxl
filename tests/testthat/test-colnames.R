beea252eac9ac2a953d120df12a66f400d882410
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-08 17:46:52 +0200

    fix test title

--- tests/testthat/test-colnames.R
+++ tests/testthat/test-colnames.R
@@ -1,6 +1,6 @@
 context("Colnames")
 
-test_that("dates respsect worksheet date sheeting", {
+test_that("auto-generated column names are the same for xls and xlsx files", {
   xls <- read_xls("missing-values.xls", col_names = FALSE)
   xlsx <- read_xlsx("missing-values.xlsx", col_names = FALSE)
 
