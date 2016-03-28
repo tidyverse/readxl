a89810b39f544d9b3ae4b6001f8af185e7aa5fe7
hadley <h.wickham@gmail.com>
2016-03-28 13:49:03 -0500

    Update test for testthat 1.0.0

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -1,9 +1,9 @@
 context("Dates")
 
 test_that("dates respsect worksheet date sheeting", {
-  d1900 <- read_xls("dates-1900.xls", col_names = FALSE)
-  d1904 <- read_xls("dates-1904.xls", col_names = FALSE)
+  d1900 <- read_xls(test_path("dates-1900.xls"), col_names = FALSE)
+  d1904 <- read_xls(test_path("dates-1904.xls"), col_names = FALSE)
 
   expect_equal(d1900, d1904)
-  expect_equal(d1900$X1, as.POSIXct(as.Date("2000-01-01")))
+  expect_equal(d1900$X1, ISOdate(2000, 01, 01, 0, tz = "UTC"))
 })
