e1df55177aae1e62c8dff1c97a2dec1a4099c2d8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-17 15:58:30 +0200

    add explicit test for return type

---
+++ tests/testthat/test-return.R
@@ -0,0 +1,17 @@
+context("read_excel")
+
+test_that("return type for xlsx files", {
+  types <- read_excel("missing-values.xlsx")
+  expect_is(types, "tbl_df")
+  expect_is(types, "tbl")
+  expect_is(types, "data.frame")
+  expect_false(tibble::has_rownames(types))
+})
+
+test_that("return type for xls files", {
+  types <- read_excel("missing-values.xls")
+  expect_is(types, "tbl_df")
+  expect_is(types, "tbl")
+  expect_is(types, "data.frame")
+  expect_false(tibble::has_rownames(types))
+})
