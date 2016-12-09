7f7c1b8419a3d81275f81bd42043b2f5610efc42
burchill <burchill@users.noreply.github.com>
2016-12-09 02:33:15 -0500

    another approach

--- src/XlsxWorkSheet.h
+++ src/XlsxWorkSheet.h
@@ -88,14 +88,14 @@ public:
         rapidxml::xml_node<>* current_node = getColumn(row, first_coord.second);
         for (int c_i = first_coord.second; c_i <= second_coord.second; c_i++) {
             
-//           rapidxml::xml_node<> *cloned_node = sheetXml_.clone_node( base_node );
-//           row->insert_node(cloned_node,current_node);
-//           rapidxml::xml_node<>* next_node = current_node->next_sibling("c");
-//           row->remove_node(current_node);
-//           rapidxml::xml_node<>* current_node = next_node 
-//           rapidxml::xml_node<>* current_node = current_node->next_sibling("c");
-          sheetXml_->clone_node(base_node, current_node);
+          rapidxml::xml_node<> *cloned_node = sheetXml_.clone_node( base_node );
+          row->insert_node(cloned_node,current_node);
+          rapidxml::xml_node<>* next_node = current_node->next_sibling("c");
+          row->remove_node(current_node);
+          rapidxml::xml_node<>* current_node = next_node 
           rapidxml::xml_node<>* current_node = current_node->next_sibling("c");
+//           sheetXml_.clone_node(base_node, current_node);
+//           rapidxml::xml_node<>* current_node = current_node->next_sibling("c");
         }
         rapidxml::xml_node<>* row = row->next_sibling("r");     
       }
