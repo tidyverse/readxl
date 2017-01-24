15b71dc4aa1b066c9dc51105a05f8e84e5b2a8f4
Jirka Lewandowski <jirka.lewandowski@posteo.de>
2017-01-25 00:25:30 +0100

    Also test for error reading extension-free xls with read_xlsx()
    as suggested by https://github.com/hadley/readxl/pull/100#pullrequestreview-18249630

--- tests/testthat/test-read-excel.R
+++ tests/testthat/test-read-excel.R
@@ -24,6 +24,7 @@ test_that("can read file without ending", {
   file.copy("iris-excel.xls", "iris-xls-no-ending")
   on.exit(file.remove("iris-xls-no-ending"))
   x <- read_xls("iris-xls-no-ending")
+  expect_error(read_xlsx("iris-xls-no-ending"))
 
 
 })
