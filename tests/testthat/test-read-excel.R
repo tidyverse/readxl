3dcd428d7e399efcb728bb88f415e29254f6591d
Jirka Lewandowski <jirka.lewandowski@posteo.de>
2017-01-25 00:49:17 +0100

    Content comparison in test for reading files without extension
    as suggested by https://github.com/hadley/readxl/pull/100#pullrequestreview-18250132

--- tests/testthat/test-read-excel.R
+++ tests/testthat/test-read-excel.R
@@ -16,14 +16,18 @@ test_that("can read sheets with inlineStr", {
 
 test_that("can read file without ending", {
 
+  ## Test for xlsx without ending
   file.copy("iris-excel.xlsx", "iris-xlsx-no-ending")
   on.exit(file.remove("iris-xlsx-no-ending"))
-  x <- read_xlsx("iris-xlsx-no-ending")
+  iris_xlsx <- read_xlsx("iris-xlsx-no-ending")
+  expect_equal(iris_xlsx, read_excel("iris-excel.xlsx"))
   expect_error(read_xls("iris-xlsx-no-ending"))
 
+  ## Test for xls without ending
   file.copy("iris-excel.xls", "iris-xls-no-ending")
   on.exit(file.remove("iris-xls-no-ending"))
-  x <- read_xls("iris-xls-no-ending")
+  iris_xls <- read_xls("iris-xls-no-ending")
+  expect_equal(iris_xls, read_excel("iris-excel.xls"))
   expect_error(read_xlsx("iris-xls-no-ending"))
 
 
