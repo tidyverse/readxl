ac0ce58d05266285dd2572e2a019c7aa32b7d708
jennybc <jenny@stat.ubc.ca>
2017-01-26 13:24:47 -0800

    Assume all suggested packages are installed for testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,4 @@
 library(testthat)
 library(readxl)
 
-if (requireNamespace("rprojroot")) {
-  test_check("readxl")
-} else {
-  message("Install suggested package rprojroot in order to run the tests.")
-}
-
-
+test_check("readxl")
