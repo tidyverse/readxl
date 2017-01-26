50e3d7613fb6cc3ad5bf08cf95a7e29dbe6d7502
Jim Hester <james.f.hester@gmail.com>
2017-01-26 09:14:26 -0500

    Do not use variadic macros
    They are a C99 extension which then requires =c++11 / c++0x causing
    other issues (#176) as we do not actually use the CXX1X compiler.
    
    The only use of fprintf in ole.c uses two additional arguments, so we
    will just hard code that number directly.
    
    Fixes #176

--- src/cran.h
+++ src/cran.h
@@ -4,7 +4,12 @@
 /* Mask printf for CMD check */
 #include <Rinternals.h>
 #define printf Rprintf
-#define fprintf(err, ...) Rprintf(__VA_ARGS__)
+
+/* Rather than use variadic macros (which are a C99 feature) we are going to
+ * hard code a macro with only two additional arguments, as the only usage of
+ * fprintf uses two additional arguments.
+ */
+#define fprintf(err, msg, a1, a2) Rprintf(msg, a1, a2)
 #define exit assert
 #undef assert
 #define assert(X) if (X) {}
