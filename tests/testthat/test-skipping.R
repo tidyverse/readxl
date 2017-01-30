ccb1c0f948a2b9070bbd507240267fb9be3ab7e4
jennybc <jenny@stat.ubc.ca>
2017-01-30 12:12:21 -0800

    Make skipping tests more challenging (blank row btwn col names and data, plus another embedded blank row)

--- tests/testthat/test-skipping.R
+++ tests/testthat/test-skipping.R
@@ -2,8 +2,10 @@ context("Skipping")
 
 skipping <- test_sheet("skipping.xlsx")
 df <- tibble::tribble(~ var1, ~ var2,
-                          11,     12,
-                          21,     22)
+                         NA,     NA,
+                      "v2,1", "v2,2",
+                          NA,     NA,
+                      "v4,1", "v4,2")
 
 test_that("leading blank rows are implicitly skipped", {
   out <- read_excel(skipping, sheet = "two_blank_rows")
@@ -22,16 +24,16 @@ test_that("leading blank rows can be implicitly AND explicitly skipped", {
 
 test_that("failure to skip junk leads to garbage df but no error", {
   out <- read_excel(skipping, sheet = "occupied_row_and_blank_row")
-  expect_identical(out$`blah blah`, c(NA, "var1", "11", "21"))
-  expect_identical(out[[2]], c(NA, "var2", "12", "22"))
+  expect_identical(out$`blah blah`, c(NA, "var1", NA, "v2,1", NA, "v4,1"))
+  expect_identical(out[[2]], c(NA, "var2", NA, "v2,2", NA, "v4,2"))
 
   out <- read_excel(skipping, sheet = "two_occupied_rows")
-  expect_identical(out$`blah blah`, c(NA, "var1", "11", "21"))
-  expect_identical(out[[2]], c("yada yada", "var2", "12", "22"))
+  expect_identical(out$`blah blah`, c(NA, "var1", NA, "v2,1", NA, "v4,1"))
+  expect_identical(out[[2]], c("yada yada", "var2", NA, "v2,2", NA, "v4,2"))
 
   out <- read_excel(skipping, sheet = "two_occupied_rows", skip = 1)
-  expect_identical(out[[1]], c("var1", "11", "21"))
-  expect_identical(out$`yada yada`, c("var2", "12", "22"))
+  expect_identical(out[[1]], c("var1", NA, "v2,1", NA, "v4,1"))
+  expect_identical(out$`yada yada`, c("var2", NA, "v2,2", NA, "v4,2"))
 })
 
 test_that("explicit skip of leading junk, implicit skip of blank rows", {
