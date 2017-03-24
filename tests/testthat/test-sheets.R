4a8d171029e28bf1c4a45f985dde03960d27bf19
jennybc <jenny@stat.ubc.ca>
2017-03-23 17:37:03 -0700

    Add more tests re: encoding
    Inspired by investigations re: #125

--- tests/testthat/test-sheets.R
+++ tests/testthat/test-sheets.R
@@ -1,15 +1,5 @@
 context("Sheets")
 
-test_that("excel_sheets returns utf-8 encoded text", {
-  sheets <- excel_sheets(test_sheet("utf8-sheets.xlsx"))
-  expect_equal(Encoding(sheets), rep("UTF-8", 2))
-  expect_equal(sheets, c("\u00b5", "\u2202"))
-
-  # Equivalent code with xls fails for reasons that I don't understand.
-  # maybe libxls isn't reencording? It's a bit suspicious that mu is
-  # \u00b5 and libxls is giving \xb5
-})
-
 test_that("informative error when requesting non-existent sheet by name", {
   expect_error(
     read_excel(test_sheet("iris-excel.xlsx"), sheet = "tulip"),
