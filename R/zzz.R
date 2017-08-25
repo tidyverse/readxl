801913b341f9336994342a9e0935db67019d0ea0
Jim Hester <james.f.hester@gmail.com>
2017-08-25 09:23:19 -0400

    Simplify condition and update to current completeme
    We no longer need to set the environment or return a logical, just
    return the completions directly.

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,10 @@
 # from https://github.com/jimhester/readxl/blob/87b4d03fcb1952ef75cc5359cf2e0bf936a7abbd/R/example.R
+#' @importFrom completeme return_unless current_function inside_quotes
 readxl_example_completer <- function(env) {
-  if (completeme::current_function(env) == "readxl_example" && completeme::inside_quotes(env)) {
+  return_unless(current_function(env) == "readxl_example" && inside_quotes(env))
 
-    # Completion within readxl_example uses the example filenames
-    comps <- grep(paste0("^\"?", env$token, "."), readxl_example(), value = TRUE)
-    env[["fileName"]] <- comps
-    env[["comps"]] <- comps
-    return (TRUE)
-  }
-  return (FALSE)
+  # Completion within readxl_example uses the example filenames
+  grep(paste0("^\"?", env$token, "."), readxl_example(), value = TRUE)
 }
 
 .onLoad <- function(pkg, lib) {
