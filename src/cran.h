45710a0fb2442eb5099165d346d5e60fadfff91f
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-04-13 16:54:25 -0700

    Reinstate the fprintf macro ... several of them!

--- src/cran.h
+++ src/cran.h
@@ -5,6 +5,15 @@
 #include <Rinternals.h>
 #define printf Rprintf
 
+/* Rather than use variadic macros (which are a C99 feature) we are going to
+ * hard code macros with different numbers of arguments
+ */
+#define fprintf2(err, msg) Rprintf(msg)
+#define fprintf3(err, msg, a1) Rprintf(msg, a1)
+#define fprintf4(err, msg, a1, a2) Rprintf(msg, a1, a2)
+#define fprintf5(err, msg, a1, a2, a3) Rprintf(msg, a1, a2, a3)
+#define fprintf10(err, msg, a1, a2, a3, a4, a5, a6, a7, a8) Rprintf(msg, a1, a2, a3, a4, a5, a6, a7, a8)
+
 #undef assert
 #define assert(X) if (X) {}
 #define exit assert
