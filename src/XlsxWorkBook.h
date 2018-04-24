9ed28d01c828bcba9f041dfd000a98cb00cbdc94
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-04-23 23:07:29 -0700

    Don't push empty string onto shared string table
    Fixes #463

--- src/XlsxWorkBook.h
+++ src/XlsxWorkBook.h
@@ -260,8 +260,9 @@ private:
     for (rapidxml::xml_node<>* string = sst->first_node();
          string; string = string->next_sibling()) {
       std::string out;
-      parseString(string, &out);    // missing strings are treated as empty
-      stringTable_.push_back(out);
+      if (parseString(string, &out)) { // missing strings are treated as empty
+        stringTable_.push_back(out);
+      }
     }
   }
 
