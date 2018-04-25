043c0064e70abcae3835d66818b909e6b4fcf165
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-04-25 13:32:37 -0700

    Update comment to actually match code

--- src/cran.h
+++ src/cran.h
@@ -1,14 +1,9 @@
 #ifndef __READ_XLS_CRAN_H
 #define __READ_XLS_CRAN_H
 
-/* Mask printf for CMD check */
+/* Mask printf and fprintf for CMD check */
 #include <Rinternals.h>
 #define printf Rprintf
-
-/* Rather than use variadic macros (which are a C99 feature) we are going to
- * hard code macros with different numbers of arguments
- */
-
 
 #ifdef __cplusplus
 extern "C" {
