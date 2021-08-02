d91fafa8a01e863cc900fb9c70176eb5ae4ddea5
Jim Hester <james.f.hester@gmail.com>
2021-08-02 08:25:12 -0400

    Support installation and loading on windows and solaris
    Older versions of the windows C runtime don't have _create_locale, so
    instead we use setlocale and the reset it to the previous locale.
    
    On solaris we just do nothing to change the locale.

--- src/libxls/locale.c
+++ src/libxls/locale.c
@@ -32,27 +32,32 @@
 #include <stdlib.h>
 #include "libxls/locale.h"
 
-#define GCC_VERSION (__GNUC__ * 10000 \
-                     + __GNUC_MINOR__ * 100 \
-                     + __GNUC_PATCHLEVEL__)
+#if defined(__MINGW32__)
+  static char* old_locale;
+#endif
 
 xls_locale_t xls_createlocale() {
-#if defined(__MINGW32__) && GCC_VERSION <= 40903
-    xls_locale_t loc = {0};
-    return loc;
+#if defined(__MINGW32__)
+    old_locale = setlocale(LC_CTYPE, ".65001");
+    return NULL;
 #elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     return _create_locale(LC_CTYPE, ".65001");
+#elif defined(__sun)
+    return NULL;
 #else
     return newlocale(LC_CTYPE_MASK, "C.UTF-8", NULL);
 #endif
 }
 
 void xls_freelocale(xls_locale_t locale) {
+#if defined(__MINGW32__)
+    setlocale(LC_ALL, old_locale);
+    return;
+#endif
+
     if (!locale)
         return;
-#if defined(__MINGW32__)
-    return;
-#elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     _free_locale(locale);
 #else
     freelocale(locale);
@@ -60,7 +65,9 @@ void xls_freelocale(xls_locale_t locale) {
 }
 
 size_t xls_wcstombs_l(char *restrict s, const wchar_t *restrict pwcs, size_t n, xls_locale_t loc) {
-#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
+#if defined(__MINGW32__)
+    return wcstombs(s, pwcs, n);
+#elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     return _wcstombs_l(s, pwcs, n, loc);
 #elif defined(HAVE_WCSTOMBS_L)
     return wcstombs_l(s, pwcs, n, loc);
