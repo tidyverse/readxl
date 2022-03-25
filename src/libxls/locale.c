86ec0d09b579eff707c11042029316e232ae9209
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-03-25 16:01:15 -0700

    Revert to locale.c from libxls v1.6.2

--- src/libxls/locale.c
+++ src/libxls/locale.c
@@ -32,53 +32,29 @@
 #include <stdlib.h>
 #include "libxls/locale.h"
 
-#ifdef __sun
-#include <string.h>
-#endif
-
-#if defined(__MINGW32__)
-  static char* old_locale;
-#endif
-
 xls_locale_t xls_createlocale() {
-#if defined(__MINGW32__)
-    old_locale = setlocale(LC_CTYPE, ".65001");
-    return NULL;
-#elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     return _create_locale(LC_CTYPE, ".65001");
-#elif defined(__sun)
-    return NULL;
 #else
     return newlocale(LC_CTYPE_MASK, "C.UTF-8", NULL);
 #endif
 }
 
 void xls_freelocale(xls_locale_t locale) {
-#if defined(__MINGW32__)
-    setlocale(LC_ALL, old_locale);
-    return;
-#endif
-
     if (!locale)
         return;
 #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     _free_locale(locale);
-#elif defined(__sun)
-   return;
 #else
     freelocale(locale);
 #endif
 }
 
 size_t xls_wcstombs_l(char *restrict s, const wchar_t *restrict pwcs, size_t n, xls_locale_t loc) {
-#if defined(__MINGW32__)
-    return wcstombs(s, pwcs, n);
-#elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64) || defined(WINDOWS)
     return _wcstombs_l(s, pwcs, n, loc);
 #elif defined(HAVE_WCSTOMBS_L)
     return wcstombs_l(s, pwcs, n, loc);
-#elif defined(__sun)
-    return wcstombs(s, pwcs, n);
 #else
     locale_t oldlocale = uselocale(loc);
     size_t result = wcstombs(s, pwcs, n);
