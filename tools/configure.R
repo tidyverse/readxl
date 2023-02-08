e5213ccd5c9f22c9e65ae1ffa73e1dd73c76d890
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-02-08 10:53:22 -0800

    Don't add CXX_STD if already there

--- tools/configure.R
+++ tools/configure.R
@@ -5,9 +5,13 @@ if (getRversion() < '3.6') {
   txt <- readLines(makevars)
   txt_win <- readLines(makevars_win)
 
-  txt <- c("CXX_STD = CXX11", txt)
-  txt_win <- c("CXX_STD = CXX11", txt_win)
+  if (!any(grepl("^CXX_STD", txt))) {
+    txt <- c("CXX_STD = CXX11", txt)
+    cat(txt, file = makevars, sep = "\n")
+  }
 
-  cat(txt, file = makevars, sep = "\n")
-  cat(txt_win, file = makevars_win, sep = "\n")
+  if (!any(grepl("^CXX_STD", txt_win))) {
+    txt_win <- c("CXX_STD = CXX11", txt_win)
+    cat(txt_win, file = makevars_win, sep = "\n")
+  }
 }
