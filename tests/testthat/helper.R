0108c9eedd5b82c1661d65c46c4cca5d0e8f783b
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2023-02-07 18:47:30 -0800

    Use `testthat::expect_no_error()` (#711)
    * Test drive expect_without_error()
    
    * Use released testthat
    
    * Function name changed before release

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,7 +1,3 @@
 ## usage:
 ## test_sheet("blanks.xls")
 test_sheet <- function(fname) testthat::test_path("sheets", fname)
-
-expect_error_free <- function(...) {
-  expect_error(..., regexp = NA)
-}
